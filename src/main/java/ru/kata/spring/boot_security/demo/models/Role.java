4dfd9a43da5f9475077ae796dc9073b562cd9242
Artem <faroeislands777870@gmail.com>
2024-07-02 23:17:15 +0300

    Add Roles to view

--- src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -55,4 +55,10 @@ public class Role implements GrantedAuthority {
     public String getAuthority() {
         return getName();
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
+
